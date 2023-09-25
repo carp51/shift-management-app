a1772f01eee731b202a7afb66931a82b7cdce98b
carp51 <kitami2250@gmail.com>
2023-09-25 18:49:17 +0900

    userにカラムを追加

---
+++ database/migrations/2023_09_25_093532_add_column_role_to_users.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('role')->comment('役割');
+            $table->string('store_id')->comment('店舗ID');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role');
+            $table->dropColumn('store_id');
+        });
+    }
+};
