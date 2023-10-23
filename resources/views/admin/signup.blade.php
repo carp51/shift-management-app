95bff0fd9b8feed918fa5fd3afe0111badcf75a9
carp51 <kitami2250@gmail.com>
2023-10-24 01:05:12 +0900

    update：機能修正（バグではない） ログイン画面をbootstrapで整える

--- resources/views/admin/signup.blade.php
+++ resources/views/admin/signup.blade.php
@@ -1,23 +1,43 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
     <title>登録画面</title>
 </head>
-<body>
-    <h1>登録画面</h1>
-    <form action="" method="post">
-        @csrf
-        <label for="email">店舗名</label>
-        <input type="text" name="store" id="store">
-        <label for="name">ユーザー名</label>
-        <input type="text" name="name" id="name">
-        <label for="password">パスワード</label>
-        <input type="password" name="password" id="password">
-        <button type="submit">送信</button>
-    </form>
+
+<body class="bg-light">
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        <h1 class="text-center">登録画面</h1>
+                    </div>
+                    <div class="card-body">
+                        <form action="" method="post">
+                            @csrf
+                            <div class="form-group">
+                                <label for="store">店舗名</label>
+                                <input type="text" class="form-control" name="store" id="store" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="name">ユーザー名</label>
+                                <input type="text" class="form-control" name="name" id="name" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="password">パスワード</label>
+                                <input type="password" class="form-control" name="password" id="password" required>
+                            </div>
+                            <button type="submit" class="btn btn-primary btn-block">送信</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -1,19 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
     <title>ログイン画面</title>
+    <style>
+        /* 追加のスタイルをここに追加できます */
+    </style>
 </head>
-<body>
-<h1>ログイン画面</h1>
-    <form action="" method="post">
-        @csrf
-        <label for="name">ユーザーネーム</label>
-        <input type="name" name="name" id="name">
-        <label for="password">パスワード</label>
-        <input type="password" name="password" id="password">
-        <button type="submit">送信</button>
-    </form>
+
+<body class="bg-light">
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        <h1 class="text-center">ログイン画面</h1>
+                    </div>
+                    <div class="card-body">
+                        <form action="" method="post">
+                            @csrf
+                            <div class="form-group">
+                                <label for="name">ユーザーネーム</label>
+                                <input type="text" class="form-control" name="name" id="name" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="password">パスワード</label>
+                                <input type="password" class="form-control" name="password" id="password" required>
+                            </div>
+                            <button type="submit" class="btn btn-primary btn-block">送信</button>
+                        </form>
+                        <div class="mt-3 text-center">
+                            <p>管理者の方は新規登録ができます。 <br><a href="/admin/signup">新規登録</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
