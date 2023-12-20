f7820d663519c3e2af76038c1a15a67a325940b8
carp51 <kitami2250@gmail.com>
2023-12-21 02:34:26 +0900

    add：新規（ファイル）機能追加
    新規登録するときのバリデーションを追加

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,10 +7,27 @@ use App\Models\User;
 use App\Models\Store;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 
 class AdminController extends Controller
 {
+    protected function validator(Request $request)
+     {
+        return Validator::make($request->all(), [
+            'name' => ['required', 'string', 'max:255'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'username' => ['required', 'string', 'max:32', 'unique:users,username', 'regex:/^[a-zA-Z0-9-_]+$/'],
+        ]);
+
+        //バリデーションエラー時のリダイレクト先（登録画面）
+        if ($validator->fails()) {
+            return redirect('/admin/signup')
+                ->withInput()
+                ->withErrors($validator);
+            }
+     }
+
     public function showSignupForm()
    {
        return view('admin.signup');
@@ -18,6 +35,19 @@ class AdminController extends Controller
 
    public function signup(Request $request)
    {
+        $validator = Validator::make($request->all(), [
+            'name' => ['required', 'string', 'max:255'],
+            'password' => ['required', 'string', 'min:8'],
+            'username' => ['required', 'string', 'max:32', 'unique:users,username', 'regex:/^[a-zA-Z0-9-_]+$/'],
+        ]);
+
+        //バリデーションエラー時のリダイレクト先（登録画面）
+        if ($validator->fails()) {
+            return redirect('/admin/signup')
+            ->withErrors($validator)  // エラーメッセージをセッションに保存
+            ->withInput();  // 入力データをセッションに保存
+        }
+
        $admin = User::query()->create([
            'name'=>$request['name'],
            'email'=>NULL,
@@ -25,6 +55,7 @@ class AdminController extends Controller
            # 新規登録できるのはadminのみなので
            'role'=>'admin',
            'store_id'=>0,
+           'username'=>$request['username']
        ]);
 
        $store = Store::query()->create([
--- resources/views/admin/users_managemant.blade.php
+++ resources/views/admin/users_managemant.blade.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>index</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<body>
-@include("components.header")
 
-<div id="app" class="p-5">
+<body>
+    @include("components.header")
+
+    <div id="app" class="p-5">
         {{ \Illuminate\Support\Facades\Auth::user()->store->name }}のメンバー
         <!-- 一覧表示するブロック ① -->
         <div v-if="state=='index'">
@@ -20,11 +22,13 @@
                 <thead>
                     <tr>
                         <th>名前</th>
+                        <th>名前</th>
                         <th>役割</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr v-for="user in users">
+                        <td v-text="user.name"></td>
                         <td v-text="user.name"></td>
                         <td v-text="user.role"></td>
                         <td class="text-right">
@@ -44,13 +48,18 @@
                 <input type="text" class="form-control" v-model="params.name">
             </div>
             <div class="form-group">
-                <label>メールアドレス</label>
-                <input type="text" class="form-control" v-model="params.email">
+                <label>ユーザーID</label>
+                <input type="text" id="username" class="form-control" name="username" v-model="params.username">
+                <!-- Vue.jsでのエラーメッセージ表示 -->
+                <span class="valid-feedback" v-if="errors" v-if="errors.username">OK</span>
+                <span class="invalid-feedback" v-if="errors && errors.username" v-text="errors.username[0]"></span>
             </div>
             <div class="bg-light px-3 py-2 mb-3" v-if="state == 'edit'">以下は省略可</div>
             <div class="form-group">
                 <label>パスワード</label>
-                <input type="password" class="form-control" v-model="params.password">
+                <input type="password" id="password" class="form-control" name="password" v-model="params.password">
+                <span class="valid-feedback" v-if="errors" v-if="errors.password">OK</span>
+                <span class="invalid-feedback" v-if="errors && errors.password" v-text="errors.password[0]"></span>
             </div>
             <div class="form-group">
                 <label>パスワード（確認）</label>
@@ -63,7 +72,6 @@
     <script src="https://cdn.jsdelivr.net/npm/vue@2.6.11"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.2/axios.min.js"></script>
     <script>
-
         new Vue({
             el: '#app',
             data: {
@@ -73,54 +81,101 @@
                     name: '',
                     email: '',
                     password: '',
-                    passwordConfirmation: ''
+                    passwordConfirmation: '',
+                    username: ''
                 },
                 users: [
                     // ユーザーデータをJSON化 ④
                     @foreach($users as $user)
-                    {!! $user !!},
+                    {!! $user!!},
                     @endforeach
-                ]
+                ],
+            errors: null,
             },
             methods: {
-                changeState(state, value) { // 状態を変化させて表示を切り替え ⑤
+            changeState(state, value) { // 状態を変化させて表示を切り替え ⑤
 
-                    if(state === 'create') {
+                if (state === 'create') {
 
-                        this.params = {
-                            id: -1,
-                            name: '',
-                            email: '',
-                            password: '',
-                            passwordConfirmation: ''
-                        };
+                    this.params = {
+                        id: -1,
+                        name: '',
+                        email: '',
+                        password: '',
+                        passwordConfirmation: '',
+                        username: ''
+                    };
 
-                    } else if(state === 'edit') {
+                } else if (state === 'edit') {
 
-                        this.params = value;
+                    this.params = value;
 
-                    }
+                }
 
-                    this.state = state;
+                this.state = state;
 
-                },
-                onSave() { // データ保存（追加＆変更） ⑥
+            },
+            onSave() { // データ保存（追加＆変更） ⑥
 
-                    const params = this.params;
-                    let url = '/admin/users';
-                    let method = 'POST';
+                const params = this.params;
+                let url = '/admin/users';
+                let method = 'POST';
 
-                    if(this.state === 'edit') { // 変更の場合
+                if (this.state === 'edit') { // 変更の場合
 
-                        url += '/'+ this.params.id;
-                        method = 'PUT';
+                    url += '/' + this.params.id;
+                    method = 'PUT';
 
-                    }
+                }
 
-                    axios({ url, method, params })
+                axios({ url, method, params })
+                    .then(response => {
+                        if (response.data.length == 0) {
+
+                            location.reload(); // 再読み込み
+                        } else {
+                            this.errors = response.data;
+                            if (!this.errors) {
+                                console.error("Error data is undefined.");
+                            } else {
+                                console.log(this.errors);
+                                // alert(this.errors.username[0]);
+                                const elm_username = $('#username');
+                                const elm_pass = $('#password');
+
+                                console.log('username' in this.errors);
+                                console.log('password' in this.errors);
+
+
+                                if ('username' in this.errors) {
+                                    elm_username.removeClass("is-valid");
+                                    elm_username.addClass("is-invalid");
+                                } else {
+                                    elm_username.removeClass("is-invalid");
+                                    elm_username.addClass("is-valid");
+                                }
+
+                                if ('password' in this.errors) {
+                                    elm_pass.removeClass("is-valid");
+                                    elm_pass.addClass("is-invalid");
+                                } else {
+                                    elm_pass.removeClass("is-invalid");
+                                    elm_pass.addClass("is-valid");
+                                }
+                            }
+                        }
+                    });
+
+            },
+            onDelete(user) { // データ削除 ⑦
+
+                if (confirm('削除します。よろしいですか？')) {
+
+                    const url = '/admin/users/' + user.id;
+                    axios.delete(url)
                         .then(response => {
 
-                            if(response.data.result === true) {
+                            if (response.data.result === true) {
 
                                 location.reload(); // 再読み込み
 
@@ -128,29 +183,15 @@
 
                         });
 
-                },
-                onDelete(user) { // データ削除 ⑦
-
-                    if(confirm('削除します。よろしいですか？')) {
-
-                        const url = '/admin/users/'+ user.id;
-                        axios.delete(url)
-                            .then(response => {
-
-                                if(response.data.result === true) {
-
-                                    location.reload(); // 再読み込み
-
-                                }
-
-                            });
-
-                    }
-
                 }
+
             }
+
+
+        }
         });
 
     </script>
-    </body>
+</body>
+
 </html>
