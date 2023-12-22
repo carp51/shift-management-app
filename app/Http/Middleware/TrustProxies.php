e0f56cd5d43250a37cdddb6bee450cfe14e00161
carp51 <kitami2250@gmail.com>
2023-12-22 21:18:27 +0900

    HerokuでHTTP（保護されていない通信）にリダイレクトされてしまう問題を解決

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    protected $proxies = '*'; //ここに' = *'を追加するのみ！
 
     /**
      * The headers that should be used to detect proxies.
